18b83f65307ebcfc18a8c03c43ea2fcf65a6a77c
maximiliano degue <maximiliano.degue@gmail.com>
2026-08-12 16:03:23 +0200

    Added encription to bucket

--- 01-s3-bucket/main.tf
+++ 01-s3-bucket/main.tf
@@ -21,10 +21,24 @@ resource "aws_s3_bucket" "this" {
 # Resource type: aws_s3_bucket_versioning
 # - References the bucket above via its id attribute (aws_s3_bucket.this.id).
 # - Status should be "Enabled".
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
 # TODO 3: server-side encryption
 # Resource type: aws_s3_bucket_server_side_encryption_configuration
 # - Simplest valid choice: SSE-S3, i.e. sse_algorithm "AES256" (no KMS key needed).
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
 # TODO 4: bucket policy denying unencrypted uploads
 # Resource type: aws_s3_bucket_policy
