60cd077e137c1905adb49654a10420f4a2e8a21a
maximiliano degue <maximiliano.degue@gmail.com>
2026-08-13 09:19:06 +0200

    Api gateway hooked up

--- 02-memo-writer/apigateway.tf
+++ 02-memo-writer/apigateway.tf
@@ -15,3 +15,40 @@
 #    function instead of a bucket. Without it: the API returns 500s while
 #    your Lambda logs show nothing at all, because it was never invoked.
 #    You may choose to discover this empirically first. It builds character.
+
+resource "aws_apigatewayv2_api" "memo_api" {
+  name          = "memo-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "memo_api_proxy" {
+  api_id                 = aws_apigatewayv2_api.memo_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.memo_writer.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "memo_api_post" {
+  api_id    = aws_apigatewayv2_api.memo_api.id
+  route_key = "POST /memo"
+  target    = "integrations/${aws_apigatewayv2_integration.memo_api_proxy.id}"
+}
+
+resource "aws_apigatewayv2_route" "memo_api_get" {
+  api_id    = aws_apigatewayv2_api.memo_api.id
+  route_key = "GET /memo/{id}"
+  target    = "integrations/${aws_apigatewayv2_integration.memo_api_proxy.id}"
+}
+
+resource "aws_apigatewayv2_stage" "memo_api" {
+  api_id      = aws_apigatewayv2_api.memo_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "memo_api" {
+  function_name = aws_lambda_function.memo_writer.function_name
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.memo_api.execution_arn}/*/*"
+}
